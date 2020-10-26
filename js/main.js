20f8f4acd37daa9ae28fcf1c20c68fc4d05a5513
Niseme <71128250+Niseme@users.noreply.github.com>
2020-10-26 13:21:41 +0200

    calculus

---
+++ js/main.js
@@ -0,0 +1,11 @@
+import { add } from './add.js';
+import { multiply } from './multiply.js'
+import { divide } from './divide.js'
+import { subtraction } from './subtract.js';
+
+
+
+console.log(add(2, 5));
+console.log(multiply(2, 5));
+console.log(divide(10, 5));
+console.log(subtraction(10, 2));
